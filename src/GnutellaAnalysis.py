c780d8984498597d1e3efb8d1762f07275421f93
Jan Pfeifer <janfred.pfeifer@gmail.com>
2023-11-03 14:46:11 +0100

    added barebones jupiter-notebook

--- src/GnutellaAnalysis.py
+++ src/GnutellaAnalysis.py
@@ -1,7 +1,22 @@
-from exercise_1.src.GraphInput import TxtFileGraphInput
+import os
+from GraphInput import TxtFileGraphInput
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+gnutella_file_path = os.path.join(
+    script_dir, "../data/p2p-Gnutella09/p2p-Gnutella09.txt"
+)
 
 input = TxtFileGraphInput()
-gnutella_file_path = "exercise_1/data/p2p-Gnutella09/p2p-Gnutella09.txt"
 input.read_input_file_and_convert(gnutella_file_path, directed=True)
-print("Gnutella09 density: ", input.computed_sensity())
+
+# Calculations:
+# Power-law properties
+# Characteristic path length
+# Density | von Luca
+# Average clustering coefficient and distribution of clustering coefficient
+# Distribution of Cohesiveness
+# Edge persistence under greedy attack (removing nodes with highest node degree first)
+# Resilience/Survivability against random and targeted attacks (greedy attack)
+
+print("Gnutella09 density: ", input.computed_density())
 print("Gnutella09 average path length: ", input.compute_average_path_length())
--- src/GraphInput.py
+++ src/GraphInput.py
@@ -11,23 +11,24 @@ def validate_graph_exists(func, *args, **kwargs):
         return func(*args, **kwargs)
 
     return wrapper
-class GraphInput(abc.ABC):
 
+
+class GraphInput(abc.ABC):
     def __init__(self):
         self._graph = None
-
 
     @abc.abstractmethod
     def read_input_file_and_convert(self, filename) -> igraph.Graph:
         pass
 
     @validate_graph_exists
-    def computed_sensity(self) -> float:
+    def computed_density(self) -> float:
         return self._graph.density()
 
     @validate_graph_exists
     def compute_average_path_length(self) -> float:
         return self._graph.average_path_length()
+
 
 class CsvFileGraphInput(GraphInput):
     def __init__(self):
@@ -42,6 +43,12 @@ class TxtFileGraphInput(GraphInput):
         super().__init__()
 
     def read_input_file_and_convert(self, filename, directed=False) -> None:
-        edges = [(int(line.split()[0]), int(line.split()[1])) for line in open(filename) if not line.startswith("#")]
+        edges = [
+            (int(line.split()[0]), int(line.split()[1]))
+            for line in open(filename)
+            if not line.startswith("#")
+        ]
         self._graph = igraph.Graph(edges=edges, directed=directed)
-        self._graph.delete_vertices([v.index for v in self._graph.vs if v.degree() == 0])
+        self._graph.delete_vertices(
+            [v.index for v in self._graph.vs if v.degree() == 0]
+        )
