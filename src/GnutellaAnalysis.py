bb60dbccde71b7b7636a688e1ec4b28f529b6e6c
Jan Pfeifer <janfred.pfeifer@gmail.com>
2023-11-03 15:46:49 +0100

    added calculation of average clustering coefficient

--- src/GnutellaAnalysis.py
+++ src/GnutellaAnalysis.py
@@ -11,12 +11,24 @@ input.read_input_file_and_convert(gnutella_file_path, directed=True)
 
 # Calculations:
 # Power-law properties
-# Characteristic path length
-# Density | von Luca
-# Average clustering coefficient and distribution of clustering coefficient
+# Characteristic path length | *von Luca implementiert*
+# Density | *von Luca implementiert*
+# Average clustering coefficient
+# Distribution of clustering coefficient
 # Distribution of Cohesiveness
 # Edge persistence under greedy attack (removing nodes with highest node degree first)
 # Resilience/Survivability against random and targeted attacks (greedy attack)
 
-print("Gnutella09 density: ", input.computed_density())
-print("Gnutella09 average path length: ", input.compute_average_path_length())
+# Done
+# print("Gnutella09 average path length: ", input.compute_average_path_length())
+# print("Gnutella09 density: ", input.computed_density())
+
+# Todo
+print(
+    "Gnutella09 Average clustering coefficient: ",
+    input.compute_average_clustering_coefficient(),
+)
+# print("Gnutella09 Distribution of clustering coefficient: ", input.todo())
+# print("Gnutella09 Distribution of cohesiveness: ", input.todo())
+# print("Gnutella09 Edge persistence under greedy attack: ", input.todo())
+# print("Gnutella09 Resilience/Survivability: ", input.todo())
--- src/GraphInput.py
+++ src/GraphInput.py
@@ -1,7 +1,6 @@
-import abc
-
+import os
 import igraph
-from igraph import *
+import abc
 
 
 def validate_graph_exists(func, *args, **kwargs):
@@ -28,6 +27,49 @@ class GraphInput(abc.ABC):
     @validate_graph_exists
     def compute_average_path_length(self) -> float:
         return self._graph.average_path_length()
+
+    @validate_graph_exists
+    def compute_average_clustering_coefficient(self) -> float:
+        # The average clustering coefficient (C_avg) of a graph can be calculated using the following formula:
+        # C_avg = (1 / n) * Σ Ci
+
+        # To calculate the clustering coefficient (Ci) for an individual node, you can use the following formula:
+        # Ci = (2 * Ei) / (ki * (ki - 1))
+        # clustering_coefficient = 2 * number of edges between neighbors of i / ((degree of i) * (degree of i-1))
+
+        total_number_of_nodes = len(self._graph.vs)
+        total_clustering_coefficient = 0.0
+
+        for node in self._graph.vs:
+            neighbors = node.neighbors()
+            number_of_neighbors = len(neighbors)
+            if number_of_neighbors < 2:
+                continue  # Skip nodes with degree less than 2 to avoid division by zero
+
+            edges_between_neighbors = self.count_edges_between_neighbors(
+                node, neighbors
+            )
+
+            # Calculate clustering_coefficient for each node
+            clustering_coefficient = (2 * edges_between_neighbors) / (
+                number_of_neighbors * (number_of_neighbors - 1)
+            )
+            total_clustering_coefficient += clustering_coefficient
+
+        # Calculate average clustering_coefficient
+        average_clustering_coefficient = (
+            total_clustering_coefficient / total_number_of_nodes
+        )
+        return average_clustering_coefficient
+
+    def count_edges_between_neighbors(self, node, neighbors):
+        edge_count = 0
+
+        for neighbor in neighbors:
+            if self._graph.are_connected(node.index, neighbor.index):
+                edge_count += 1
+
+        return edge_count
 
 
 class CsvFileGraphInput(GraphInput):
