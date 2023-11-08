46854a9f2ce70cf2e25ed799f9fff5d5d0300ed7
Jan Pfeifer <janfred.pfeifer@gmail.com>
2023-11-08 16:16:23 +0100

    refactored the code and added comments

--- src/GraphInput.py
+++ src/GraphInput.py
@@ -1,5 +1,4 @@
 import copy
-import os
 import random
 from typing import Optional, List, Dict, Iterable
 import igraph
@@ -8,6 +7,7 @@ import abc
 from collections import Counter
 
 
+# Checks for the initialization of a graph
 def validate_graph_exists(func, *args, **kwargs):
     def wrapper(*args, **kwargs):
         if args[0]._graph is None:
@@ -17,6 +17,7 @@ def validate_graph_exists(func, *args, **kwargs):
     return wrapper
 
 
+# This class contains the calculation of the metrics
 class GraphInput(abc.ABC):
     def __init__(self):
         self._graph: Optional[igraph.Graph] = None
@@ -25,32 +26,28 @@ class GraphInput(abc.ABC):
     def read_input_file_and_convert(self, filename) -> igraph.Graph:
         pass
 
+    # Metric 1: Density
     @validate_graph_exists
     def computed_density(self) -> float:
         return self._graph.density()
 
+    # Metric 2: Average path length
     @validate_graph_exists
     def compute_average_path_length(self) -> float:
         return self._graph.average_path_length()
 
+    # Metric 3, 4: Clustering coefficient and its distribution
     @validate_graph_exists
     def compute_clustering_coefficient(self) -> (float, Dict[float, int]):
-        # Calculate the average clustering coefficient (C_avg) of a graph and its distribution.
-        # C_avg = (1 / n) * Σ Ci
-        # Ci = (2 * Ei) / (ki * (ki - 1))
-        # clustering_coefficient = 2 * number of edges between neighbors of i / ((degree of i) * (degree of i-1))
-
         total_number_of_nodes = len(self._graph.vs)
         total_clustering_coefficient = 0.0
-        clustering_coefficients = (
-            []
-        )  # List to store clustering coefficients for all nodes
+        clustering_coefficients = []
 
         for node in self._graph.vs:
             neighbors = node.neighbors()
             number_of_neighbors = len(neighbors)
             if number_of_neighbors < 2:
-                continue  # Skip nodes with a degree less than 2 to avoid division by zero
+                continue
 
             edges_between_neighbors = self.count_edges_between_neighbors(
                 node, neighbors
@@ -58,49 +55,22 @@ class GraphInput(abc.ABC):
 
             # Calculate clustering_coefficient for each node
             clustering_coefficient = (2 * edges_between_neighbors) / (
-                    number_of_neighbors * (number_of_neighbors - 1)
+                number_of_neighbors * (number_of_neighbors - 1)
             )
             total_clustering_coefficient += clustering_coefficient
-
-            # Append the clustering coefficient to the list
             clustering_coefficients.append(clustering_coefficient)
 
         # Calculate average clustering_coefficient
         average_clustering_coefficient = (
-                total_clustering_coefficient / total_number_of_nodes
+            total_clustering_coefficient / total_number_of_nodes
         )
-
-        # Calculate the distribution of clustering coefficients
         clustering_coefficient_distribution = Counter(clustering_coefficients)
 
         return average_clustering_coefficient, clustering_coefficient_distribution
 
-    def count_edges_between_neighbors(self, node, neighbors):
-        edge_count = 0
-
-        for neighbor in neighbors:
-            if self._graph.are_connected(node.index, neighbor.index):
-                edge_count += 1
-
-        return edge_count
-
+    # Metric 5: Distribution of the cohesiveness
     @validate_graph_exists
-    def compute_cpl_for_all_nodes(self) -> List[float]:
-        cpls = []
-        for node in self._graph.vs:
-            cpl = self.compute_characteristic_path_length(node)
-            cpls.append(cpl)
-        return cpls
-
-    @validate_graph_exists
-    def compute_characteristic_path_length(self, node) -> float:
-        shortest_paths = self._graph.shortest_paths(node, mode=igraph.ALL)
-        cpl_values = [sum(sp) for sp in shortest_paths if sp != float("inf")]
-
-        return sum(cpl_values) / len(cpl_values) if cpl_values else 0.0
-
-    @validate_graph_exists
-    def compute_cohesiveness_for_each_node(self) -> Dict[int, int]:
+    def compute_cohesiveness_distribution(self) -> Dict[int, int]:
         cohesiveness_values = {}
 
         for node in self._graph.vs:
@@ -119,8 +89,11 @@ class GraphInput(abc.ABC):
 
         return cohesiveness_values
 
+    # Metric 6: Edge persistence under greedy attack
     @validate_graph_exists
-    def compute_edge_persistence_under_greedy_attack(self, runs_without_result=10, total_number_of_runs=100) -> Dict:
+    def compute_edge_persistence_under_greedy_attack(
+        self, runs_without_result=10, total_number_of_runs=100
+    ) -> Dict:
         graph = copy.deepcopy(self._graph)
         diameters = {}
         diameters[0] = graph.diameter()
@@ -133,8 +106,11 @@ class GraphInput(abc.ABC):
                 diameters[proportional_removed_nodes] = graph.diameter()
         return diameters
 
+    # Metric 7: Edge persistence under random attack
     @validate_graph_exists
-    def compute_edge_persistence_under_random_attack(self, runs_without_result=10, total_number_of_runs=100) -> Dict:
+    def compute_edge_persistence_under_random_attack(
+        self, runs_without_result=10, total_number_of_runs=100
+    ) -> Dict:
         graph = copy.deepcopy(self._graph)
         diameters = {}
         random.seed(42)
@@ -148,7 +124,18 @@ class GraphInput(abc.ABC):
                 diameters[proportional_removed_nodes] = graph.diameter()
         return diameters
 
+    # Helper for the calculation of the clustering coefficient
+    def count_edges_between_neighbors(self, node, neighbors):
+        edge_count = 0
 
+        for neighbor in neighbors:
+            if self._graph.are_connected(node.index, neighbor.index):
+                edge_count += 1
+
+        return edge_count
+
+
+# CSV File Reader
 class CsvFileGraphInput(GraphInput):
     def __init__(self):
         super().__init__()
@@ -169,6 +156,7 @@ class CsvFileGraphInput(GraphInput):
         )
 
 
+# Text File Reader
 class TxtFileGraphInput(GraphInput):
     def __init__(self):
         super().__init__()
